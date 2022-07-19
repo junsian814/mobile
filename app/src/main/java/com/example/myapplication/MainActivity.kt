80095aea9930d059a1935ab7a765dbb0c12cb9e8
TARUC <leejs-wm19@student.tarc.edu.my>
2022-07-19 14:39:47 +0800

    new thing

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImg : ImageView
     lateinit var numberText : TextView
-    lateinit var nameText : TextView
+    lateinit var nameText : EditText
     lateinit var playerNameText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +34,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateName(view: View){
         playerNameText.text = nameText.text
+
+        nameText.text.clear()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun rollDice(){
